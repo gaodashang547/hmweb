d3b28f4d898cd2ecfaaf07f187d19f28e4d323cf
feng <547190485@qq.com>
2019-08-23 16:24:56 +0800

    登陆样式完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/views/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    { name: 'login', path: '/login', component: Login }
+    // 配置登陆组件的路由规则
   ]
 })
---
+++ src/views/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div>
+      <van-nav-bar title="登陆" />
+      <van-cell-group>
+        <van-field v-model="user.mobile" required clearable label="手机" placeholder="请输入手机号" />
+
+        <van-field v-model="user.code" label="验证码" placeholder="请您输入验证码" required />
+      </van-cell-group>
+      <div class="btn">
+        <van-button type="info" style="width:100%">登陆</van-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+.van-nav-bar {
+  background: blue;
+}
+.van-nav-bar__title {
+  color: white;
+}
+.btn {
+  padding: 20px;
+}
+</style>
